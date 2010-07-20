cb6b1957e16f42e3bfec6b027f5e3beb8d9352d3
Mariano Reingart <reingart@gmail.com>
2010-07-20 17:29:44 -0300

    Initial import.
    Some files forked from PyAfipWs project:
     * http://code.google.com/p/pyafipws/source/browse/simplexml.py?r=02976ca9d703697beb3ea139dd66cf844e73d67c
     * http://code.google.com/p/pyafipws/source/browse/soap.py?r=065807825fbde4d1f44a6721d7c2d7e33ac81f5e

---
+++ server.py
@@ -0,0 +1,316 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+"Simple SOAP Server implementation"
+
+# WARNING: EXPERIMENTAL PROOF-OF-CONCEPT IN EARLY DEVELOPMENT STAGE 
+# TODO:
+# * Refactory: cleanup and remove duplicates between server ad client 
+# * Generalize harcoded code
+# * Handle and Enforce SoapAction, namespaces, and exceptions
+# * Generate a WSDL suitable for testing with SoapUI
+
+__author__ = "Mariano Reingart (mariano@nsis.com.ar)"
+__copyright__ = "Copyright (C) 2010 Mariano Reingart"
+__license__ = "LGPL 3.0"
+__version__ = "0.1"
+
+from simplexml import SimpleXMLElement
+
+DEBUG = False
+
+class SoapFault(RuntimeError):
+    def __init__(self,faultcode,faultstring):
+        self.faultcode = faultcode
+        self.faultstring = faultstring
+
+# soap protocol specification & namespace
+soap_namespaces = dict(
+    soap11="http://schemas.xmlsoap.org/soap/envelope/",
+    soap="http://schemas.xmlsoap.org/soap/envelope/",
+    soapenv="http://schemas.xmlsoap.org/soap/envelope/",
+    soap12="http://www.w3.org/2003/05/soap-env",
+)
+
+class SoapDispatcher(object):
+    "Simple Dispatcher for SOAP Server"
+    
+    def __init__(self, soap_ns='soap11', namespace=None, prefix=False, **kwargs):
+        self.methods = {}
+        self.soap_ns = soap_ns
+        self.soap_uri = soap_namespaces[soap_ns] # soap napespace
+        self.namespace = namespace
+        self.prefix = prefix
+    
+    def register_function(self, method, function, returns, args):
+        self.methods[method] = function, returns, args
+        
+    def dispatch(self, xml):
+        "Receive and proccess SOAP call"
+        request = SimpleXMLElement(xml, namespace=self.namespace)
+        
+        # parse request message and get local method
+        method = request['soap:Body'].children()[0]
+        if DEBUG: print "dispatch method", method.getName()
+        function, returns_types, args_types = self.methods[method.getName()]
+    
+        # de-serialize parameters
+        args = method.children().unmarshall(args_types)
+ 
+        # execute function
+        ret = function(**args)
+        if DEBUG: print ret
+        
+        # build response message
+        xml = """<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+<soap:Body><%(method)sResponse xmlns="%(namespace)s"/></soap:Body>
+</soap:Envelope>"""  % {'method':method.getName(), 'namespace':self.namespace}
+        response = SimpleXMLElement(xml, namespace=self.namespace)   
+
+        # serialize returned values (response)
+        response['%sResponse' % method.getName()].marshall(returns_types.keys()[0], ret)
+        
+        return response.asXML()
+
+    # Introspection functions:
+
+    def list_methods(self):
+        "Return a list of aregistered operations"
+        return [(method, function.__doc__) for method, (function, returns, args) in self.methods.items()] 
+
+    def help(self, method=None):
+        "Generate sample request and response messages"
+        (function, returns, args) = self.methods[method]
+        xml = """
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+<soap:Body><%(method)s xmlns="%(namespace)s"/></soap:Body>
+</soap:Envelope>"""  % {'method':method, 'namespace':self.namespace}
+        request = SimpleXMLElement(xml, namespace=self.namespace, prefix=self.prefix)
+        for k,v in args.items():
+            request[method].marshall(k, v, add_comments=True, ns=False)
+
+        xml = """
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+<soap:Body><%(method)sResponse xmlns="%(namespace)s"/></soap:Body>
+</soap:Envelope>"""  % {'method':method, 'namespace':self.namespace}
+        response = SimpleXMLElement(xml, namespace=self.namespace, prefix=self.prefix)
+        for k,v in returns.items():
+            response['%sResponse'%method].marshall(k, v, add_comments=True, ns=False)
+
+        return request.asXML(pretty=True), response.asXML(pretty=True), function.__doc__
+
+
+    def wsdl(self):
+        "Generate Web Service Description v1.1"
+        xml = """<?xml version="1.0"?>
+<definitions name="StockQuote" targetNamespace="http://example.com/sample.wsdl"
+          xmlns:tns="http://example.com/sample.wsdl"
+          xmlns:xsd1="http://example.com/sample.xsd"
+          xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/"
+          xmlns="http://schemas.xmlsoap.org/wsdl/">
+
+    <types>
+       <schema targetNamespace="http://example.com/stockquote.wsdl"
+              elementFormDefault="qualified"
+              xmlns="http://www.w3.org/2000/10/XMLSchema">
+       </schema>
+    </types>
+
+</definitions>
+"""
+        wsdl = SimpleXMLElement(xml)
+
+        for method, (function, returns, args) in self.methods.items():
+            # create elements:
+                
+            def parseElement(name, values, array=True):
+                element = wsdl['types']['schema'].addChild('element')
+                element.addAttribute('name', name)
+                complex = element.addChild("complexType")
+                if not array:
+                    all = complex.addChild("all")
+                else:
+                    all = complex.addChild("sequence")
+                for k,v in values:
+                    e = all.addChild("element")
+                    e.addAttribute('name', k)
+                    if array:
+                        e.addAttribute('minOccurs',"0")
+                        e.addAttribute('maxOccurs',"1")
+                    if v in (int, str, float, bool, unicode):
+                        type_map={str:'string',bool:'boolean',int:'integer',float:'float',unicode:'string'}
+                        t=type_map[v]
+                    elif isinstance(v, list):
+                        n="tns:ArrayOf%s%s" % (name, k)
+                        l = []
+                        for d in v:
+                            l.extend(d.items())
+                        parseElement(n, l, array=True)
+                        t = n
+                    elif isinstance(v, dict): 
+                        n="tns:%s%s" % (name, k)
+                        parseElement(n, v.items())
+                        t = n
+                    e.addAttribute('type', t)
+            
+            parseElement("%sRequest" % method, args.items())
+            parseElement("%sResponse" % method, returns.items())
+
+            # create messages:
+            for m,e in ('Input','Request'), ('Output','Response'):
+                message = wsdl.addChild('message')
+                message.addAttribute('name', "%s%s" % (method, m))
+                part = message.addChild("part")
+                part.addAttribute('name', 'body')
+                part.addAttribute('element', 'tns:%s%s' % (method,e))
+
+        # create ports
+        portType = wsdl.addChild('portType')
+        portType.addAttribute('name', "StockQuotePortType")
+        for method in self.methods.keys():
+            op = portType.addChild('operation')
+            op.addAttribute('name', method)
+            input = op.addChild("input")
+            input.addAttribute('message', "tns:%sInput" % method)
+            output = op.addChild("input")
+            output.addAttribute('message', "tns:%sOutput" % method)
+
+        # create bindings
+        binding = wsdl.addChild('binding')
+        binding.addAttribute('name', "StockQuoteSoapBinding")
+        binding.addAttribute('type', "tns:StockQuotePortType")
+        soapbinding= binding.addChild('soap:binding')
+        soapbinding.addAttribute('style',"document")
+        soapbinding.addAttribute('transport',"http://schemas.xmlsoap.org/soap/http")
+        for method in self.methods.keys():
+            op = binding.addChild('operation')
+            op.addAttribute('name', method)
+            soapop = binding.addChild('soap:operation')
+            soapop.addAttribute('soapAction', "http://example.com/GetLastTradePrice")
+            input = op.addChild("input")
+            soapbody = input.addChild("soap:body")
+            soapbody.addAttribute("use","literal")
+            output = op.addChild("input")
+            soapbody = output.addChild("soap:body")
+            soapbody.addAttribute("use","literal")
+
+        service = wsdl.addChild('service')
+        service.addAttribute("name", "StockQuoteService")
+        service.addChild('documentation', text="my first web service")
+        port=service.addChild('port')
+        port.addAttribute("name","StockQuotePort")
+        port.addAttribute("binding","tns:StockQuoteBinding")
+        soapaddress = port.addChild('soap:address')
+        soapaddress.addAttribute("location","http://example.com/stockquote")
+        return wsdl.asXML(pretty=True)
+    
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class SOAPHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        "User viewable help information and wsdl"
+        args = self.path[1:].split("?")
+        if self.path != "/" and args[0] not in self.server.dispatcher.list_methods():
+            self.send_error(404, "Method not found")
+        else:
+            if self.path == "/":
+                # return wsdl if no method supplied
+                response = self.server.dispatcher.wsdl()
+            else:
+                # return supplied method help (?request or ?response messages)
+                req, res = self.server.dispatcher.help(args[0])
+                if len(args)==1 or args[1]=="request":
+                    response = req
+                else:
+                    response = res                
+            self.send_response(200)
+            self.send_header("Content-type", "text/xml")
+            self.end_headers()
+            self.wfile.write(response)
+
+    def do_POST(self):
+        "SOAP POST gateway"
+        self.send_response(200)
+        self.send_header("Content-type", "text/xml")
+        self.end_headers()
+        request = self.rfile.read(int(self.headers.getheader('content-length')))
+        response = self.server.dispatcher.dispatch(request)
+        self.wfile.write(response)
+
+
+if __name__=="__main__":
+    import sys
+
+    dispatcher = SoapDispatcher(
+        location = "http://localhost:8008/",
+        action = 'http://localhost:8008/', # SOAPAction
+        namespace = "http://example.com/sample.wsdl", prefix="ns0",
+        trace = True,
+        ns = True)
+    
+    def adder(p,c):
+        print c[0]['d'],c[1]['d'],
+        return {'ab': p['a']+p['b'], 'dd': c[0]['d']+c[1]['d']}
+    
+    dispatcher.register_function('Adder', adder,
+        returns={'AddResult': {'ab': int, 'dd': str } }, 
+        args={'p': {'a': int,'b': int}, 'c': [{'d':str}]})
+
+    if '--local' in sys.argv:
+        # dummy local test
+        xml = """<?xml version="1.0" encoding="UTF-8"?> 
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+       <soap:Body>
+         <adder xmlns="http://example.com/sample.wsdl">
+           <p>
+            <a>1</a>
+            <b>2</b>
+           </p>
+           <c>
+            <d>hola</d>
+            <d>chau</d>
+           </c>
+        </adder>
+       </soap:Body>
+    </soap:Envelope>"""
+
+        print dispatcher.dispatch(xml)
+
+        for method, doc in dispatcher.list_methods():
+            request, response, doc = dispatcher.help(method)
+            print request
+            print response
+            
+        print dispatcher.wsdl()
+
+    if '--serve' in sys.argv:
+        print "Starting server..."
+        httpd = HTTPServer(("", 8008), SOAPHandler)
+        httpd.dispatcher = dispatcher
+        httpd.serve_forever()
+
+    if '--consume' in sys.argv:
+        from client import SoapClient
+        client = SoapClient(
+            location = "http://localhost:8008/",
+            action = 'http://localhost:8008/', # SOAPAction
+            namespace = "http://example.com/sample.wsdl", 
+            soap_ns='soap',
+            trace = True,
+            ns = False)
+        response = client.Adder(p={'a':1,'b':2},c=[{'d':'hola'},{'d':'chau'}])
+        result = response.AddResult
+        print int(result.ab)
+        print str(result.dd)
+
+
---
+++ simplexml.py
@@ -0,0 +1,249 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+"Simple XML manipulation"
+
+__author__ = "Mariano Reingart (mariano@nsis.com.ar)"
+__copyright__ = "Copyright (C) 2008/009 Mariano Reingart"
+__license__ = "LGPL 3.0"
+__version__ = "1.0"
+
+import xml.dom.minidom
+
+
+DEBUG = False
+
+
+class SimpleXMLElement(object):
+    "Simple XML manipulation (simil PHP)"
+    
+    def __init__(self, text = None, elements = None, document = None, namespace = None, prefix=None):
+        self.__ns = namespace
+        self.__prefix = prefix
+        if text:
+            try:
+                self.__document = xml.dom.minidom.parseString(text)
+            except:
+                if DEBUG: print text
+                raise
+            self.__elements = [self.__document.documentElement]
+        else:
+            self.__elements = elements
+            self.__document = document
+    
+    def addChild(self,name,text=None,ns=True):
+        "Adding a child tag to a node"
+        if not ns or not self.__ns:
+            if DEBUG: print "adding %s ns %s %s" % (name, self.__ns,ns)
+            element = self.__document.createElement(name)
+        else:
+            if DEBUG: print "adding %s ns %s %s" % (name, self.__ns,ns)
+            element = self.__document.createElementNS(self.__ns, "%s:%s" % (self.__prefix, name))
+        if text:
+            if isinstance(text, unicode):
+                element.appendChild(self.__document.createTextNode(text))
+            else:
+                element.appendChild(self.__document.createTextNode(str(text)))
+        self.__element.appendChild(element)
+        return SimpleXMLElement(
+                    elements=[element],
+                    document=self.__document,
+                    namespace=self.__ns,
+                    prefix=self.__prefix)
+    
+    def __setattribute__(self, name, text):
+        "Add text child tag node (short form)"
+        self.addChild(name,text)
+
+    def addComment(self, data):
+        "Add an xml comment to this child"
+        comment = self.__document.createComment(data)
+        self.__element.appendChild(comment)
+
+    def asXML(self,filename=None,pretty=False):
+        "Return the XML representation of the document"
+        if not pretty:
+            return self.__document.toxml('UTF-8')
+        else:
+            return self.__document.toprettyxml(encoding='UTF-8')
+
+    def __repr__(self):
+        "Return the XML representation of this tag"
+        return self.__element.toxml('UTF-8')
+
+    def getName(self):
+        "Return the tag name of this node"
+        return self.__element.tagName
+    
+    def attributes(self):
+        "Return a dict of attributes for this tag"
+        #TODO: use slice syntax [:]?
+        return self.__element.attributes
+
+    def addAttribute(self, name, value):
+        "Set an attribute value from a string"
+        #TODO: use __setitem__?
+        self.__element.setAttribute(name, value)
+     
+    def __getattr__(self, name):
+        "Search (even in child nodes) and return a child tag by name"
+        try:
+            if self.__ns:
+                if DEBUG: print "searching %s by ns=%s" % (name,self.__ns)
+                elements = self.__elements[0].getElementsByTagNameNS(self.__ns, name)
+            if not self.__ns or not elements:
+                if DEBUG: print "searching %s " % (name)
+                elements = self.__elements[0].getElementsByTagName(name)
+            if not elements:
+                if DEBUG: print self.__elements[0].toxml()
+                raise AttributeError("Sin elementos")
+            return SimpleXMLElement(
+                elements=elements,
+                document=self.__document,
+                namespace=self.__ns,
+                prefix=self.__prefix)
+        except AttributeError, e:
+            raise AttributeError("Tag not found: %s (%s)" % (name, str(e)))
+
+    def __iter__(self):
+        "Iterate over xml tags at this level"
+        try:
+            for __element in self.__elements:
+                yield SimpleXMLElement(
+                    elements=[__element],
+                    document=self.__document,
+                    namespace=self.__ns,
+                    prefix=self.__prefix)
+        except:
+            raise
+
+    def __getitem__(self,item):
+        "Return xml tag (by name o numerical index)"
+        #TODO: use string names for real tag attributes?
+        if isinstance(item, basestring):
+            return getattr(self,item)
+        else:
+            return SimpleXMLElement(
+                elements=[self.__elements[item]],
+                document=self.__document,
+                namespace=self.__ns,
+                prefix=self.__prefix)
+
+    def __dir__(self):
+        "List xml children tags names"
+        return [node.tagName for node 
+                in self.__element.childNodes
+                if node.nodeType != node.TEXT_NODE]
+
+    def children(self):
+        "Return xml children tags element"
+        return SimpleXMLElement(
+                elements=[__element for __element in self.__element.childNodes
+                          if __element.nodeType == __element.ELEMENT_NODE],
+                document=self.__document,
+                namespace=self.__ns,
+                prefix=self.__prefix)
+
+    def __contains__( self, item):
+        "Search for a tag name in this element or child nodes"
+        return self.__element.getElementsByTagName(item)
+    
+    def __unicode__(self):
+        "Returns the unicode text nodes of the current element"
+        if self.__element.childNodes:
+            rc = u""
+            for node in self.__element.childNodes:
+                if node.nodeType == node.TEXT_NODE:
+                    rc = rc + node.data
+            return rc
+        return ''
+    
+    def __str__(self):
+        "Returns the str text nodes of the current element"
+        return unicode(self).encode("utf8","ignore")
+
+    def __int__(self):
+        "Returns the integer value of the current element"
+        return int(self.__str__())
+
+    def __float__(self):
+        "Returns the float value of the current element"
+        try:
+            return float(self.__str__())
+        except:
+            raise IndexError(self.__element.toxml())    
+    
+    __element = property(lambda self: self.__elements[0])
+
+    def unmarshall(self, types):
+        "Convert to python values the current serialized xml element"
+        # types is a dict of {tag name: convertion function}
+        # example: types={'p': {'a': int,'b': int}, 'c': [{'d':str}]}
+        #   expected xml: <p><a>1</a><b>2</b></p><c><d>hola</d><d>chau</d>
+        #   returnde value: {'p': {'a':1,'b':2}, `'c':[{'d':'hola'},{'d':'chau'}]}
+        d = {}
+        for node in self:
+            name = str(node.getName())
+            if isinstance(types[name],list):
+                value = []
+                for child in node.children():
+                    value.append(child.unmarshall(types[name][0]))
+            elif isinstance(types[name],dict):
+                value = node.children().unmarshall(types[name])
+            else:
+                if str(node):
+                    value = types[name](str(node))
+                else:
+                    value = None
+            d[name] = value
+        return d
+
+    def marshall(self, name, value, add_child=True, add_comments=False, ns=False):
+        "Analize python value and add the serialized XML element using tag name"
+        if isinstance(value, dict):  # serialize dict (<key>value</key>)
+            child = add_child and self.addChild(name,ns=ns) or self
+            for k,v in value.items():
+                child.marshall(k, v, add_comments=add_comments, ns=ns)
+        elif isinstance(value, tuple):  # serialize tuple (<key>value</key>)
+            child = add_child and self.addChild(name,ns=ns) or self
+            for k,v in value:
+                getattr(self,name).marshall(k, v, add_comments=add_comments, ns=ns)
+        elif isinstance(value, list): # serialize lists
+            child=self.addChild(name,ns=ns)
+            if add_comments:
+                child.addComment("Repetitive array of:")
+            for t in value:
+                child.marshall(name,t, False, add_comments=add_comments, ns=ns)
+        elif isinstance(value, basestring): # do not convert strings or unicodes
+            self.addChild(name,value,ns=ns)
+        elif value in (int, str, float, bool, unicode):
+            # add commented placeholders for simple tipes (for examples/help only)
+            type_map={str:'string',unicode:'string',
+                      bool:'boolean',int:'integer',float:'float'}
+            child = self.addChild(name,ns=ns) 
+            child.addComment(type_map[value])
+        else: # the rest of object types are converted to string 
+            self.addChild(name,str(value),ns=ns) # check for a asXML?
+
+
+if __name__ == "__main__":
+    span = SimpleXMLElement('<span><a href="google.com">google</a><prueba><i>1</i><float>1.5</float></prueba></span>')
+    print str(span.a)
+    print int(span.prueba.i)
+    print float(span.prueba.float)
+
+    span = SimpleXMLElement('<span><a href="google.com">google</a><a>yahoo</a><a>hotmail</a></span>')
+    for a in span.a:
+        print str(a)
+
+    span.addChild('a','altavista')
+    print span.asXML()
