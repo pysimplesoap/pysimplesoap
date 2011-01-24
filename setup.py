f9d652136351e18bb3e5e0b0e3a15ba5bbe13c59
ralf@henschkowski.com <ralf@henschkowski.com>
2011-01-24 09:04:47 +0100

    To mke this installable via pypi:
    * Add setup.py
    * Move files to package dir

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysimplesoap',
+      version='1.0',
+      description='Python Simple SOAP Library',
+      author='Mariano Reingart',
+      author_email='reingart@gmail.com',
+      url='http://code.google.com/p/pysimplesoap',
+      packages=['pysimplesoap',],
+     )
+
