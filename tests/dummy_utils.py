3213e9d1a49f0a1534f48e7a3f3a97739f74327f
Mariano Reingart <reingart@gmail.com>
2012-01-07 02:07:05 -0300

    moved DummyHTTP to DummyTransport

--- tests/dummy_utils.py
+++ tests/dummy_utils.py
@@ -3,13 +3,5 @@ import os
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-class DummyHTTP:
-    def __init__(self, xml_response):
-        self.xml_response = xml_response
-    def request(self, location, method, body, headers):
-        print method, location
-        print headers
-        print body
-        return {}, self.xml_response
-
+from pysimplesoap.transport import DummyTransport as DummyHTTP 
 
