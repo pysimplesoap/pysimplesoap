8538d48524e9fa96d7e3f216ec852a5c52d74b53
matee <matee@matee.net>
2012-05-14 13:59:45 +0200

    better support of multiple namespaces in server

--- tests/server_multins_test.py
+++ tests/server_multins_test.py
@@ -32,9 +32,12 @@ REQ = """
    </soapenv:Body>
 </soapenv:Envelope>"""
 
+REQ1 = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Header/><soapenv:Body><p727:updateDeliveryStatus xmlns:p727="http://external.mt.moboperator"><p727:serviceMsisdn>51072</p727:serviceMsisdn><p727:serviceName>IPLA</p727:serviceName><p727:messageDeliveryStatuses><p924:items xmlns:p924="http://model.common.mt.moboperator"><p924:msisdn>48726401494</p924:msisdn><p924:status>380</p924:status><p924:deliveryId>33946812</p924:deliveryId></p924:items></p727:messageDeliveryStatuses></p727:updateDeliveryStatus></soapenv:Body></soapenv:Envelope>"""
+
 SINGLE_NS_RESP = """<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:ext="http://external.mt.moboperator" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Body><ext:activateSubscriptionsResponse><activateSubscriptionsReturn><code>0</code><description>desc</description><items><msisdn>791000000</msisdn><properties><name>x</name><value>2</value></properties><status>0</status></items></activateSubscriptionsReturn></ext:activateSubscriptionsResponse></soapenv:Body></soapenv:Envelope>"""
 
 MULTI_NS_RESP = """<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:ext="http://external.mt.moboperator" xmlns:mod="http://model.common.mt.moboperator" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Body><ext:activateSubscriptionsResponse><ext:activateSubscriptionsReturn><mod:code>0</mod:code><mod:description>desc</mod:description><mod:items><mod:msisdn>791000000</mod:msisdn><mod:properties><mod:name>x</mod:name><mod:value>2</mod:value></mod:properties><mod:status>0</mod:status></mod:items></ext:activateSubscriptionsReturn></ext:activateSubscriptionsResponse></soapenv:Body></soapenv:Envelope>"""
+MULTI_NS_RESP1 = """<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:p727="http://external.mt.moboperator" xmlns:p924="http://model.common.mt.moboperator" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Body><p727:updateDeliveryStatusResponse><p727:updateDeliveryStatusReturn><p924:code>0</p924:code><p924:description>desc</p924:description></p727:updateDeliveryStatusReturn></p727:updateDeliveryStatusResponse></soapenv:Body></soapenv:Envelope>"""
 
 class TestServerMultiNS(unittest.TestCase):
 
@@ -76,6 +79,34 @@ class TestServerMultiNS(unittest.TestCase):
         ]
     }
 
+    def _updateDeliveryStatus(self, serviceMsisdn, serviceName, messageDeliveryStatuses=[]):
+        code = 0
+        desc = 'desc'
+        return {
+            'external:updateDeliveryStatusReturn': [
+                {'model:code': code},
+                {'model:description': desc}
+            ]
+        }
+    _updateDeliveryStatus.returns = {'non-empty-dict': 1}
+    _updateDeliveryStatus.args = {
+        'serviceMsisdn': str,
+        'serviceName': str,
+        'messageDeliveryStatuses': [
+            {'items': {
+                    'msisdn': str,
+                    'status': int,
+                    'deliveryId': str,
+                    'properties': ({
+                            'name': str,
+                            'value': int
+                        },
+                    )
+                }
+            }
+        ]
+    }
+        
     def _multi_ns_func(self, serviceMsisdn, serviceName, activations=[]):
         code = 0
         desc = 'desc'
@@ -154,11 +185,13 @@ class TestServerMultiNS(unittest.TestCase):
             self._multi_ns_func,
             returns=self._multi_ns_func.returns,
             args=self._multi_ns_func.args)
-            
-        for x in range(1000):
-            dispatcher.dispatch(REQ)
+        dispatcher.register_function('updateDeliveryStatus',
+            self._updateDeliveryStatus,
+            returns=self._updateDeliveryStatus.returns,
+            args=self._updateDeliveryStatus.args)
         
         self.assertEqual(dispatcher.dispatch(REQ), MULTI_NS_RESP)
+        self.assertEqual(dispatcher.dispatch(REQ1), MULTI_NS_RESP1)
     
 if __name__ == '__main__':
     unittest.main()
