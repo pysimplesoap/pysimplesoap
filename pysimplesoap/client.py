f8a7811ca329c1cb8585fa38f4ea4fa70cbc0c31
Mariano Reingart <reingart@gmail.com>
2012-06-27 18:02:55 -0300

    version update

--- pysimplesoap/client.py
+++ pysimplesoap/client.py
@@ -15,7 +15,7 @@
 __author__ = "Mariano Reingart (reingart@gmail.com)"
 __copyright__ = "Copyright (C) 2008 Mariano Reingart"
 __license__ = "LGPL 3.0"
-__version__ = "1.06d"
+__version__ = "1.07a"
 
 TIMEOUT = 60
 
