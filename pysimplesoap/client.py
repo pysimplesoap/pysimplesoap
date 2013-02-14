24444df165b0191eb106ec5feea27f34931dff0a
Dean Gardiner <gardiner91@gmail.com>
2013-02-15 03:17:59 +1300

    Version Bump, updated [.gitignore]

--- pysimplesoap/client.py
+++ pysimplesoap/client.py
@@ -15,7 +15,7 @@
 __author__ = "Mariano Reingart (reingart@gmail.com)"
 __copyright__ = "Copyright (C) 2008 Mariano Reingart"
 __license__ = "LGPL 3.0"
-__version__ = "1.07a"
+__version__ = "1.07b"
 
 TIMEOUT = 60
 
