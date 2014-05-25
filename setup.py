c7b916b2cf7413fe9e737b512612125e99ae4dcc
Mariano Reingart <reingart@gmail.com>
2014-05-24 23:35:05 -0300

    modified setup for both names (PySimpleSOAP and soap2py) and updated version to 1.14

--- setup.py
+++ setup.py
@@ -10,15 +10,17 @@ except:
 
 from pysimplesoap import __version__, __author__, __author_email__, __license__
 
-setup(
-    name='PySimpleSOAP',
-    version=__version__,
-    description='Python Simple SOAP Library',
-    author=__author__,
-    author_email=__author_email__,
-    url='http://code.google.com/p/pysimplesoap',
-    packages=['pysimplesoap'],
-    license=__license__,
-    console=['client.py'],
-    cmdclass={"py2exe": build_installer},
-)
+# in the transition, register both:
+for name in ('soap2py', 'PySimpleSOAP'):
+    setup(
+        name=name,
+        version=__version__,
+        description='Python simple and lightweight SOAP Library',
+        author=__author__,
+        author_email=__author_email__,
+        url='http://code.google.com/p/pysimplesoap',
+        packages=['pysimplesoap'],
+        license=__license__,
+    #    console=['client.py'],
+        cmdclass={"py2exe": build_installer},
+    )
