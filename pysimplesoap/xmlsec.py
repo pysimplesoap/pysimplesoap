2c1b24bdfe86d1dc8303dc147caf6dcbae2e5201
Mariano Reingart <reingart@gmail.com>
2014-09-09 16:02:00 -0300

    Add SHA1 message digest for XML Sec

--- pysimplesoap/xmlsec.py
+++ pysimplesoap/xmlsec.py
@@ -12,12 +12,15 @@
 
 """Pythonic XML Security Library implementation"""
 
+import base64
+import hashlib
 import lxml.etree
 from cStringIO import StringIO
 
 # Features:
 #  * Uses M2Crypto and lxml (libxml2) but it is independent from libxmlsec1
 #  * Sign, Verify, Encrypt & Decrypt XML documents
+
 
 def canonicalize(xml):
     "Return the canonical (c14n) form of the xml document for hashing"
@@ -26,6 +29,12 @@ def canonicalize(xml):
     output = StringIO()
     et.write_c14n(output)
     return output.getvalue()
+
+
+def digest(payload):
+    "Create a SHA1 hash and return the base64 string"
+    return base64.b64encode(hashlib.sha1(payload).digest())
+
 
 if __name__ == "__main__":
     sample_xml = """<?xml version="1.0" encoding="UTF-8"?>
