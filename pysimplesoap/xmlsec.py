b95a1ec75dff9561c2b028564d93b41995ac3644
Mariano Reingart <reingart@gmail.com>
2014-09-09 15:25:50 -0300

    Add Canonical XML support (XML Security)

---
+++ pysimplesoap/xmlsec.py
@@ -0,0 +1,38 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+"""Pythonic XML Security Library implementation"""
+
+import lxml.etree
+from cStringIO import StringIO
+
+# Features:
+#  * Uses M2Crypto and lxml (libxml2) but it is independent from libxmlsec1
+#  * Sign, Verify, Encrypt & Decrypt XML documents
+
+def canonicalize(xml):
+    "Return the canonical (c14n) form of the xml document for hashing"
+    # UTF8, normalization of line feeds/spaces, quoting, attribute ordering...
+    et = lxml.etree.parse(StringIO(xml))
+    output = StringIO()
+    et.write_c14n(output)
+    return output.getvalue()
+
+if __name__ == "__main__":
+    sample_xml = """<?xml version="1.0" encoding="UTF-8"?>
+<Envelope xmlns="urn:envelope">
+  <Data b='a' a='b'>
+	Hello, World!
+  </Data>
+</Envelope>"""
+    print canonicalize(sample_xml)
+    
