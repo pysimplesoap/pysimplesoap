29159679cc29b3ff07cb584fe6b5ef53612622a1
Mariano Reingart <reingart@gmail.com>
2014-09-11 20:50:38 -0300

    Added tentative WSSE signer using xmlsec (RSA-SHA1)

--- pysimplesoap/wsse.py
+++ pysimplesoap/wsse.py
@@ -26,6 +26,7 @@ import hashlib
 import warnings
 
 from . import __author__, __copyright__, __license__, __version__
+from .simplexml import SimpleXMLElement
 
 
 class UsernameToken:
@@ -52,3 +53,46 @@ class UsernameToken:
         header(k)['xmlns:wsse'] = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd'
         #<wsse:UsernameToken xmlns:wsu='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd'>
 
+
+BIN_TOKEN_TMPL = """<?xml version="1.0" encoding="UTF-8"?>
+<wsse:Security soapenv:mustUnderstand="1" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+    <wsse:BinarySecurityToken EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3" wsu:Id="CertId-45851B081998E431E8132880700036719" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+%(certificate)s</wsse:BinarySecurityToken>
+    <ds:Signature Id="Signature-13" xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+        %(signed_info)s
+        <ds:SignatureValue>%(signature_value)s</ds:SignatureValue>
+        <ds:KeyInfo Id="KeyId-45851B081998E431E8132880700036720">
+            <wsse:SecurityTokenReference wsu:Id="STRId-45851B081998E431E8132880700036821" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+                <wsse:Reference URI="#CertId-45851B081998E431E8132880700036719" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"/>
+            </wsse:SecurityTokenReference>
+        </ds:KeyInfo>
+    </ds:Signature>
+</wsse:Security>
+"""
+
+class BinaryTokenSignature:
+    "WebService Security extension to add a basic signature to xml request"
+
+    def __init__(self, certificate="", private_key="", password=None):
+        # read the X509v3 certificate (PEM)
+        self.certificate = ''.join([line for line in open(certificate)
+                                         if not line.startswith("---")])
+        self.private_key = private_key
+        self.password = None
+
+    def preprocess(self, client, request, method, args, kwargs, headers, soap_uri):
+        # get xml elements:
+        body = request('Body', ns=soap_uri, )
+        header = request('Header', ns=soap_uri, )
+        # prepare body xml attributes to be signed (reference)
+        body['wsu:Id'] = "id-14"
+        body['xmlns:wsu'] = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+        ref_xml = body.as_xml()
+        # sign using RSA-SHA1 (XML Security)
+        from . import xmlsec
+        vars = xmlsec.rsa_sign_ref(ref_xml, "#id-14", 
+                                   self.private_key, self.password)
+        vars['certificate'] = self.certificate
+        # generate the xml (filling the placeholders)
+        wsse = SimpleXMLElement(BIN_TOKEN_TMPL % vars)
+        header.import_node(wsse)
