e476c1868dc43715afdb812ea40ab6c24d489de4
Kenneth Nielsen <k.nielsen81@gmail.com>
2014-12-15 17:15:03 +0100

    Remove, self=selv from lambda expression in method dispatch

--- pysimplesoap/client.py
+++ pysimplesoap/client.py
@@ -175,7 +175,7 @@ class SoapClient(object):
     def __getattr__(self, attr):
         """Return a pseudo-method that can be called"""
         if not self.services:  # not using WSDL?
-            return lambda self=self, *args, **kwargs: self.call(attr, *args, **kwargs)
+            return lambda *args, **kwargs: self.call(attr, *args, **kwargs)
         else:  # using WSDL:
             return lambda *args, **kwargs: self.wsdl_call(attr, *args, **kwargs)
 
