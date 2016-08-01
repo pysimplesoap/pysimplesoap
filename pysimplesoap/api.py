561eda1685b5122973c9118189c666ce8d1765af
Zhang Yu <yu.3.zhang@nokia.com>
2016-08-01 17:18:28 +0800

    decode support both input and output

--- pysimplesoap/api.py
+++ pysimplesoap/api.py
@@ -8,12 +8,12 @@ SOAPENV = 'http://schemas.xmlsoap.org/soap/envelope/'
 def decode(headers, content, wsdl, soap_ver='soap11'):
     (elements, messages, port_types, bindings, services) = parse(wsdl)
     response = SimpleXMLElement(content, headers=headers)
-    method = headers['soapaction'].rsplit('/', 1)[1]
+    method = headers['soapaction'].strip('"').rsplit('/', 1)[1]
 
     def get_operation():
         # try to find operation in wsdl file
         for service_name, service in services.iteritems():
-            for port_name, port in [port for port in service['ports'].items()]:
+            for port_name, port in service['ports'].iteritems():
                 if port['soap_ver'] == soap_ver:
                     if method in port['operations']:
                         return port['operations'][method]
@@ -21,8 +21,10 @@ def decode(headers, content, wsdl, soap_ver='soap11'):
         raise RuntimeError('Cannot determine service in WSDL: '
                            'SOAP version: %s' % soap_ver)
 
-    output = get_operation()['output']
-    resp = response('Body', ns=SOAPENV).children().unmarshall(output, strict=True)
+    operation = get_operation()
+    input_output = operation['input']
+    input_output.update(operation['output'])
+    resp = response('Body', ns=SOAPENV).children().unmarshall(input_output, strict=True)
 
-    return resp[output.keys()[0]]
+    return resp.values()[0]
 
