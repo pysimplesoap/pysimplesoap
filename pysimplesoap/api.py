1173367e355d6b97d9a0b0adf04205bc852f3c29
Zhang Yu <yu.3.zhang@nokia.com>
2016-08-03 11:25:52 +0800

    add mime decoding into decode interface

--- pysimplesoap/api.py
+++ pysimplesoap/api.py
@@ -1,30 +1,95 @@
 ''' API module for functional usage like encode/decode '''
+import xmltodict
 from .wsdl import parse
 from .simplexml import SimpleXMLElement
+
+__all__ = ('decode', )
+
+
+def decode(headers, content, wsdl, soap_ver='soap11'):
+    parser = _SoapMsgParser(wsdl, soap_ver)
+    return parser.parse(headers, content)
 
 
 SOAPENV = 'http://schemas.xmlsoap.org/soap/envelope/'
 
-def decode(headers, content, wsdl, soap_ver='soap11'):
-    (elements, messages, port_types, bindings, services) = parse(wsdl)
-    response = SimpleXMLElement(content, headers=headers)
-    method = headers['soapaction'].strip('"').rsplit('/', 1)[1]
+class _SoapMsgParser(object):
+    def __init__(self, wsdl, soap_ver='soap11'):
+        self.elements, self.messages, self.port_types, self.bindings, self.services = parse(wsdl)
+        self.soap_version = soap_ver
 
-    def get_operation():
-        # try to find operation in wsdl file
-        for service_name, service in services.iteritems():
+    def parse(self, headers, content):
+        content_type = headers and headers.get('content-type', '') or ''
+        raw_xml, mimes = self._get_raw_xml(content_type, content)
+        resp = self._parse_raw_xml(raw_xml, headers['soapaction'].strip('"').rsplit('/', 1)[1])
+        resp.update(self._parse_mimes(mimes))
+
+        return resp
+
+    def _get_raw_xml(self, content_type, text):
+        settings = self._parse_content_type(content_type)
+
+        if settings.get('multipart/related', False):
+            boundary = '--' + settings['boundary']
+            mimes = []
+            start_pos = text.find(boundary) + len(boundary)
+            end_pos = text.find(boundary, start_pos)
+            while end_pos > 0:
+                mimes.append(text[start_pos: end_pos].strip())
+                start_pos = end_pos + len(boundary)
+                end_pos = text.find(boundary, start_pos)
+            if not mimes:
+                return '', []
+
+            raw_xml = mimes.pop(0)
+            start_pos = raw_xml.find(settings['start'])+len(settings['start'])
+            raw_xml = raw_xml[start_pos:].strip()
+
+            return raw_xml, mimes
+
+        return text, []
+
+    def _parse_content_type(self, content_type):
+        settings = {}
+        for item in content_type.split(';'):
+            if '=' in item:
+                k, v = item.strip().split('=', 1)
+                settings[k] = v.strip('""')
+            else:
+                settings[item.strip()] = True
+
+        return settings
+
+    def _parse_raw_xml(self, raw_xml, method):
+        response = SimpleXMLElement(raw_xml)
+        operation = self._get_operation(method)
+        input_output = operation['input']
+        input_output.update(operation['output'])
+        resp = response('Body', ns=SOAPENV) \
+                .children() \
+                .unmarshall(input_output, strict=True) \
+                .values()[0]
+
+        return resp
+
+    def _parse_mimes(self, mimes):
+        return dict([self._parse_one_mime(mime) for mime in mimes])
+
+    def _parse_one_mime(self, mime):
+        cid_start = mime.find('Content-ID:') + len('Content-ID:')
+        cid_end = mime.find('\r\n', cid_start)
+        cid = mime[cid_start:cid_end].strip('< >')
+        content = mime[mime.find('\r\n\r\n', cid_end):].strip()
+
+        return cid, xmltodict.parse(content)
+
+    def _get_operation(self, method):
+        for service_name, service in self.services.iteritems():
             for port_name, port in service['ports'].iteritems():
-                if port['soap_ver'] == soap_ver:
+                if port['soap_ver'] == self.soap_version:
                     if method in port['operations']:
                         return port['operations'][method]
 
         raise RuntimeError('Cannot determine service in WSDL: '
-                           'SOAP version: %s' % soap_ver)
-
-    operation = get_operation()
-    input_output = operation['input']
-    input_output.update(operation['output'])
-    resp = response('Body', ns=SOAPENV).children().unmarshall(input_output, strict=True)
-
-    return resp.values()[0]
+                           'SOAP version: %s' % self.soap_version)
 
--- utest/test_pysimplesoap/test_api.py
+++ utest/test_pysimplesoap/test_api.py
@@ -3,11 +3,11 @@ from .base import BaseTestcase
 from pysimplesoap.api import decode
 
 
-wsdl=os.path.abspath('utest/data/register.wsdl')
+wsdl=os.path.abspath('utest/data/ne3s.wsdl')
 
 
 class TestMsgDecoding(BaseTestcase):
-    def test_decode_startRegistration_msg(self):
+    def test_decode_mime_with_xml_only_msg(self):
         headers = {
             'soapaction': 'http://www.nokiasiemens.com/ne3s/1.0/startRegistration',
             'content-type': 'multipart/related; boundary="MIMEBoundary_37b1639e1b936afbe7bfb91e329eda80160d10652a34d791"; type="text/xml"; start="<0.27b1639e1b936afbe7bfb91e329eda80160d10652a34d791@apache.org>"',
@@ -22,4 +22,16 @@ class TestMsgDecoding(BaseTestcase):
         self.assertEqual(resp['agentResponse'], 'AQI=')
         self.assertEqual(resp['licenseRequired'], False)
         self.assertEqual(resp['registrationComplete'], True)
+
+    def test_decode_multiple_mimes_msg(self):
+        headers = {'content-type': 'multipart/related; boundary=MIMEBoundaryurn_uuid_41C3C1F5C427229F7A1466676077706; type="text/xml"; start="<0.urn:uuid:41C3C1F5C427229F7A1466676077707@apache.org>"', 'soapaction': '"http://www.nokiasiemens.com/ne3s/1.0/transferNotification"','transfer-encoding':'chunked'}
+        resp = decode(headers, self.get_data('multi_mime'), wsdl=wsdl)
+        self.assertEqual(resp['queueId'], 'ne3s_atl_logUploadNotificationQueue')
+        self.assertEqual(resp['sequenceNumber'], None),
+        self.assertEqual(resp['attachmentProperties']['contentType'], 'oats')
+        self.assertIn('transferNotificationContent1466676077553@nokiasiemens.com', resp)
+        attachment = resp['transferNotificationContent1466676077553@nokiasiemens.com']
+        self.assertEqual(attachment['log']['@logFileName'], 'AUDIT_LOG')
+        self.assertEqual(attachment['log']['@logType'], 'AUDIT_LOG')
+        self.assertEqual(attachment['log']['record']['interfaceType'], 'ne3s_ws')
 
