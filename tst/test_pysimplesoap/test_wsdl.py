041b1d859e5aef12a221ef2b58401c9baf7852c4
xiao <shiliang.xiao@nokia.com>
2016-08-22 04:23:17 +0200

    add ut cases for wsal paring module

--- tst/test_pysimplesoap/test_wsdl.py
+++ tst/test_pysimplesoap/test_wsdl.py
@@ -9,4 +9,9 @@ class TestWsdlParsing(BaseTestcase):
         self.assertEqual(services.keys(), ['NE3SRegistrationService'])
         self.assertEqual(services['NE3SRegistrationService']['documentation'], '')
         self.assertEqual(services['NE3SRegistrationService']['ports']['ne3sRegistration']['operations']['startRegistration']['output']['startRegistrationResponse']['agentIdentity']['uniqueId'], str)
+    def test_with_import_and_multiservices(self):
+        _, _, _, _, services = parse(os.path.abspath('tst/data/ne3s.wsdl'))
+        self.assertEqual(services['NE3SBasicNotificationsService']['documentation'], '')
+        self.assertEqual(services['NE3SBasicNotificationsService']['ports']['ne3sBasicNotifications']['operations']['transferNotification']['output']['transferNotificationResponse']['managerRegistrationId'], str)
+        self.assertEqual(services['NE3SBulkOperationsService']['ports']['ne3sBulkOperations']['operations']['upload']['input']['upload']['sessionId'], str)
 
