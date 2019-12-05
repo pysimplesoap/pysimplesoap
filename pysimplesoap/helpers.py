d7b11857e428d2388848f6ab2dc4d640500f9c67
ajhenri <amandajohe@live.com>
2019-12-05 13:52:32 -0500

    initialize superclass for Struct obj, remove unneeded __keys declaration in init

--- pysimplesoap/helpers.py
+++ pysimplesoap/helpers.py
@@ -609,8 +609,8 @@ class Struct(dict):
     __keys = []
     
     def __init__(self, key=None):
+        super(dict, self).__init__()
         self.key = key
-        self.__keys = []
         self.array = False
         self.namespaces = {}     # key: element, value: namespace URI
         self.references = {}     # key: element, value: reference name
